29f7eecd9dea03d2e73db6901e315e936e0b2703
sharadpatel11 <54919810+sharadpatel11@users.noreply.github.com>
2024-05-02 01:22:48 -0400

    Files Added

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import './navbar.css';
+
+const Menu = () => (
+  <>
+  <p><a href="#home">Home</a></p>
+  <p><a href="#about">About</a></p>
+  <p><a href="#skills">Skills</a></p>
+  <p><a href="#projects">Projects</a></p>
+  </>
+)
+
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  return (
+    <div className="sp__top">
+      <div className="sp__navbar">
+        <div className="sp__navbar-logo">
+          <h1>SP</h1>
+        </div>
+        <div className="sp__navbar-links">
+          <Menu />
+        </div>
+        <div className="sp__navbar-welcome">
+            <h2>Welcome</h2>
+        </div>
+        <div className="sp__navbar-contact">
+          <button type="button">Contact</button>        
+        </div>
+        <div className="sp__navbar-menu">
+          {toggleMenu
+            ? <RiCloseLine color="#000000" size={27} onClick={() => setToggleMenu(false)} />
+            : <RiMenu3Line color="#000000" size={27} onClick={() => setToggleMenu(true)} />
+          }
+          {toggleMenu && (
+            <div className="sp__navbar-menu_container scale-up-center">
+              <div className="sp__navbar-menu_container-links">
+                <Menu />
+                <button type="button">Contact Us</button>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/containers/header/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './header.css';
+import resume from '../../Assets/Sharad_Patel_Resume.pdf';
+import header_image from '../../Assets/header_image.png';
+
+const Header = () => {
+  return (
+    <div className="sp__header" id="home">
+      <div className="sp__header-left">
+        <img src={header_image} alt="portrait" />
+      </div>
+      <div className="sp__header-right">
+        <div className="sp__header-right__top">
+          <h2>Hello!</h2>
+          <h1>I'm Sharad</h1>
+        </div>
+        <p>Passionate Computer Science graduate with a strong drive for solving real-world challenges through technology.</p>
+        <button><a href= { resume } download="Sharad_Patel_Resume">Download Resume</a></button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
